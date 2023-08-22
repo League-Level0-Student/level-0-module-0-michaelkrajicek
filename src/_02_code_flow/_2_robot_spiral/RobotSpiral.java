587d5c601fa527bc63abca631f048a90c3f1ef0d
student <student@192.168.1.53>
2023-08-22 14:00:40 -0700

    almost finished pentagon crazy

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -10,35 +10,36 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSpiral {
 
 	public static void main(String[] args) {
-		
+
 		// Create a new Robot
-		
+		Robot rob2 = new Robot();
 		// Set your robot's pen down 
-		
+		rob2.penDown();
 		// SPEED. Set the robot to go at max speed (100)
-
+		rob2.setSpeed(100);
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
-			//        Start its value as zero.
-
+		//        Start its value as zero.
+		int count = 0; 
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
+		while(count<50) {
 			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
-
+			rob2.setRandomPenColor();
 			// DRAW.    Move the robot (5*count) pixels
 			//          count is the name of the variable you created earlier
-
+			rob2.move(5*count);
 			// TURN.    Turn the robot (360/7) degrees to the right
-
+			rob2.turn(360/7);
 			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
-
+			count += 1;
 			// Change the robot pen width to the current value of the count variable 
+			rob2.setPenWidth(count);
 
+			//  End the while loop here
+		}
 
-		//  End the while loop here
-
-		
 		// Run the program. 
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
-		
+
+
 	}
 }
